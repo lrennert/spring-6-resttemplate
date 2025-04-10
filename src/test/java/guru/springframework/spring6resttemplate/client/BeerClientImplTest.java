0be702fc3425d24f72ccc75f8da9baed8e796e98
Lydia Rennert <lydia.rennert@gmail.com>
2025-04-10 16:54:58 +0200

    lre: Implement listBeers

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,10 +1,17 @@
 package guru.springframework.spring6resttemplate.client;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class BeerClientImplTest {
+
+    @Autowired
+    BeerClient beerClient;
 
     @Test
     void listBeers() {
+        beerClient.listBeers();
     }
 }
