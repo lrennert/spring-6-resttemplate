e4428ed95305ef6db13a4e0afca07536464dca48
Lydia Rennert <lydia.rennert@gmail.com>
2025-04-11 14:21:04 +0200

    lre: Provide base path through application.properties

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -13,7 +13,6 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class BeerClientImpl implements BeerClient {
 
-    private static final String BASE_URL = "http://localhost:8080";
     private static final String GET_BEER_PATH = "/api/v1/beer";
 
     private final RestTemplateBuilder restTemplateBuilder;
@@ -22,9 +21,9 @@ public class BeerClientImpl implements BeerClient {
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<BeerDTOPageImpl> response =
-                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, BeerDTOPageImpl.class);
+        ResponseEntity<BeerDTOPageImpl> response = restTemplate.getForEntity(
+                GET_BEER_PATH, BeerDTOPageImpl.class);
 
-        return null;
+        return response.getBody();
     }
 }
